539fe5dfca7838fe033182a3f0358c760aad27ce
Arun <aruntheprofessor@gmail.com>
2026-01-22 12:33:45 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
+  base: process.env.EMAIL_USER || "/Agrowing",
 })
